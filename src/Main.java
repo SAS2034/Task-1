6eb73737f2cde3300eec125293e52a83edd411ac
SAS2034 <sashustova@gmail.com>
2024-02-22 13:42:30 +0300

    изменила создание массива

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Random rand = new Random();
-        int array[] =new int[100];
+        int[] array = new int[100];
         for(int i = 0; i < 100; i++){
             array[i] = rand.nextInt(100);}
             System.out.println(Arrays.toString(array));
